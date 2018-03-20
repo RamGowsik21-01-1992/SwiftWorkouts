1de81abccc3193fa0b55405c575f57c864372cf7
CSSCORP <csscorp@CSSCORPs-Mac-mini.local>
2018-03-20 18:57:52 +0530

    Servide added in the framework

---
+++ SwiftWorkouts/SwiftWorkouts/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  SwiftWorkouts
+//
+//  Created by CSSCORP on 3/20/18.
+//  Copyright © 2018 CSSCORP. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    public init() {}
+    
+    public static func getUserName() -> String {
+        return "Ram Gowsik Jeyachandran"
+    }
+    
+    
+}
---
+++ SwiftWorkouts/SwiftWorkouts/SwiftWorkouts.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWorkouts.h
+//  SwiftWorkouts
+//
+//  Created by CSSCORP on 3/20/18.
+//  Copyright © 2018 CSSCORP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftWorkouts.
+FOUNDATION_EXPORT double SwiftWorkoutsVersionNumber;
+
+//! Project version string for SwiftWorkouts.
+FOUNDATION_EXPORT const unsigned char SwiftWorkoutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWorkouts/PublicHeader.h>
+
+
